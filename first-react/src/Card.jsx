f4e7e6fe39ac582d03df4f46fe4699a82b7e534a
meti2626 <metseboka@gmail.com>
2025-10-24 00:47:04 +0300

    here2

---
+++ first-react/src/Card.jsx
@@ -0,0 +1,18 @@
+import Profilepic from './assets/here.jpg';
+
+
+function Card()
+{
+   return(
+
+      <div className="card">
+           <img className="profile-pic" src={Profilepic}  alt="profile pic"></img>
+
+           <h2>Bro Code</h2>
+           <p>Learn React with Bro Code</p>
+      </div>
+
+   );
+}
+
+export default Card
