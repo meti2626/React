048cbd15975d02cd37d53fd84061b054605bd882
meti2626 <metseboka@gmail.com>
2025-10-28 21:27:28 +0300

    here

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,14 +1,29 @@
 //conditional rendering = allows you to control what gets rendered in your application  based on certain conditins (sjow,hide,or change )
 
-import UserGreeting from './UserGreeting.jsx';
+import List from './List.jsx';
 
 function App() {
+   
+
+     const fruits = [{id:1,name:"apple" ,cal:95} ,
+                   {id:2, name:"orange", cal:45},
+                   {id:3,name:"banana", cal:105}, 
+                   {id:4,name:"cocanat" , cal:159} ,
+                   {id:5,name:"pinapple" , cal: 37}];
+
+
+      const vegetable = [{id:6,name:"potato" ,cal:110} ,
+                         {id:7, name:"celery", cal:15},
+                        {id:8,name:"carrots", cal:25}, 
+                        {id:9,name:"corn" , cal: 63} ,
+                         {id:10,name:"brocolli" , cal: 50}];
+
 
 return(
  
    <>
-    <UserGreeting isLoggedIn={false} username="BroCode" />
-           
+         <List  items={fruits} category="Fruits" />
+          <List  items={vegetable}  category="vegetables"/> 
   </>
      
 
---
+++ first-react/src/List.jsx
@@ -0,0 +1,41 @@
+
+
+function List(props){
+
+// but what do we need a uique key ????????????????
+  // enclose the array elemnts to make them objects 
+   
+// see about sort () and map, arrow function
+// fruits.sort((a,b) => a.name.localeCompare(b.name));
+//fruits.sort((a,b) => b.name.localeCompare(a.name));
+ // fruits.sort((a,b) => a.cal - b.cal);      
+
+ //fruits.sort((a,b) => b.cal - a.cal);  
+
+
+ // rendering based on criteria 
+
+
+  //  const lowCalFruits = fruits.filter(fruit => fruit.cal < 100);
+
+     
+  //  const lowCalFruits = fruits.filter(fruit => fruit.cal>=100);
+
+   const category = props.category;
+
+   const itemList = props.items;
+
+
+    const listItems = itemList.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp;
+        {fruit.cal} </li>);
+
+
+    return (
+       <>
+            <h3 className="list-cat">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+      </>
+  );
+}
+
+export default List
