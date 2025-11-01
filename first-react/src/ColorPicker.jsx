5cea08299f7c2f42aa25fadc7fa513ad392537ac
meti2626 <metseboka@gmail.com>
2025-11-01 22:27:00 +0300

    nov-1-3

--- first-react/src/ColorPicker.jsx
+++ first-react/src/ColorPicker.jsx
@@ -4,14 +4,22 @@ import React, {useState} from 'react';
 
 function ColorPicker()
 {
-    const  [color, setColor] = useState("red");
+    const  [color, setColor] = useState("#FFFFFF");
 
-    return (<>
+    function handleColorChange(event) {
+          setColor(event.target.value);
+    }
+
+    return (
           
          <div  className='color-picker-container'>
 
             <h1>Color Picker</h1>
-
+            <div className="color-display"   style={{backgroundColor: color}}>
+                <p>The selected color is: {color}</p>
+            </div>
+             <label>Select a Color:</label>
+             <input type='color'   value={color}   onChange={handleColorChange}></input>
          </div>
     
     
@@ -20,7 +28,7 @@ function ColorPicker()
     
     
     
-    </>);
+ );
 }
 
 
